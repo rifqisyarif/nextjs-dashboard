9cfe3edc5f6137b67697a7aa99a44351e5b3f1aa
rifqisyarif <rifqisyarif202@gmail.com>
2024-03-13 09:33:50 +0700

    add dashboard, display invoice data and add crud on invoice page

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -8,21 +8,24 @@ import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/a
 
 
 export default async function Page() {
-   
+
     return <main>
         <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
             Dashboard
         </h1>
         <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Suspense fallback={<CardsSkeleton />}>
-          <CardWrapper />
-        </Suspense>
+            <Suspense fallback={<CardsSkeleton />}>
+                {/* @ts-expect-error Server Component */}
+                <CardWrapper />
+            </Suspense>
         </div>
         <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
             <Suspense fallback={<RevenueChartSkeleton />}>
+                {/* @ts-expect-error Server Component */}
                 <RevenueChart />
             </Suspense>
             <Suspense fallback={<LatestInvoicesSkeleton />}>
+                {/* @ts-expect-error Server Component */}
                 <LatestInvoices />
             </Suspense>
         </div>
